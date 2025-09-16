4df50c1d443c5dc35a5032162e560f7688e2d37a
kirill050103 <kirill050103@gmail.com>
2025-09-17 00:43:20 +0300

    add tasks

---
+++ python_elective/homework_2/modules.py
@@ -0,0 +1,54 @@
+def matrix_on_matrix(matrix1, matrix2):
+    res=[]
+    for row in range(len(matrix1)):
+        res.append([])
+        for column in range(len(matrix2[0])):
+            row_sum=0
+            for ind in range(len(matrix1[0])):
+                row_sum+=matrix1[row][ind] * matrix2[ind][column]
+            res[row].append(row_sum)
+    return res
+
+matrix1 = [[0,1,2],[1,0,2],[2,1,0],[0,0,1]]
+matrix2 = [[0,1],[1,0],[1,1]]
+print(matrix_on_matrix(matrix1,matrix2))
+
+def matrix_on_vec(matrix, vec):
+    res=[]
+    for i in range(len(matrix)):
+        row_sum=0
+        for j in range(len(matrix[i])):
+            row_sum+=matrix[i][j]*vec[j]
+        res.append(row_sum)
+    return res
+
+def matrix_trace(matrix):
+    res=0
+    for i in range(len(matrix)):
+        res+=matrix[i][i]
+    return res
+print(matrix_trace([[1,2,3],[4,5,6],[7,8,9]]))
+
+def scal_mul_vec(vec1, vec2, cos=None):
+    from math import cos
+    res=0
+    for i in range(len(vec1)):
+        res += vec1[i]*vec2[i]
+    res*=cos
+    return res
+
+def histogram(data, bins):
+    r=max(data)
+    l=min(data)
+    b_size=(r-l)/bins
+    hist=[""]*bins
+    for num in data:
+        b_ind = int((num - l) / b_size)
+        if b_ind==bins:
+            b_ind=bins-1
+        hist[b_ind]+='*'
+    return hist
+
+import random
+data = [random.randint(0,15) for _ in range(5)]
+print(data, '\n',histogram(data, 2))
